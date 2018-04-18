279ca25d0b6e209e6cfd78658b0ec37d84676805
Jerome Hurley <vonswirl@googlemail.com>
2018-04-18 14:32:56 +0100

    Data-table template for views created

--- controllers/angularController.js
+++ controllers/angularController.js
@@ -1,4 +1,5 @@
-var angularModule = angular.module('angularModule', []);
+angularModule = angular.module('angularModule', []);
+angular.module('showcase.withOptions', ['datatables']).controller('WithOptionsCtrl', WithOptionsCtrl);
 
 function angularControl() {
     angularModule.controller('angularController', ['$scope', function ($scope) {
@@ -7,4 +8,27 @@ function angularControl() {
 
     }]);
 }
-module.exports = { angularControl };
+
+
+
+function WithOptionsCtrl(DTOptionsBuilder, DTColumnDefBuilder) {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder.newOptions()
+        .withPaginationType('full_numbers')
+        .withDisplayLength(2)
+        .withDOM('pitrfl');
+    vm.dtColumnDefs = [
+        DTColumnDefBuilder.newColumnDef(0),
+        DTColumnDefBuilder.newColumnDef(1).notVisible(),
+        DTColumnDefBuilder.newColumnDef(2).notSortable()
+    ];
+}
+
+module.exports = { angularControl, angular };
+
+
+
+
+
+
+
